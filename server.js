2b2a2b505b492efd3640876a1ba1402090ede7a5
jessicabreedlove <jessicabreedlove@gmail.com>
2022-05-20 15:20:05 -0600

    created json data draft

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const connect = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
-app.use('/', require('./routes'));
+connect.initDatabase();
+
+app.use(bodyParser.json()).use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Content-Disposition'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+
+  next();
+});
 
 app.listen(port, () => {
-  console.log(`You have connected and are listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
