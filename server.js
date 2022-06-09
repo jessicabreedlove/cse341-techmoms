e81b416542e3e6f3a3cc16aa2c31f723ef2b0337
jessicabreedlove <jessicabreedlove@gmail.com>
2022-06-08 18:08:54 -0600

    swagger- not broke, not working

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db.js');
 
+//me trying to bridge the gap with swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 //load config
 dotenv.config({ path: './config/config.env' });
 
@@ -52,36 +56,11 @@ app.use('/auth', require('./routes/auth'));
 
 const port = process.env.PORT || 5000;
 
-//what i had before oauth video
-// const bodyParser = require('body-parser');
-// const connect = require('./db/connect');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
+//swagger to work?
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-// connect.initDatabase();
-
-// app.use(bodyParser.json()).use('/', require('./routes'));
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.get('/api-docs', swaggerUi.setup(swaggerDocument));
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-
-//   //do I even need all of these?
-//   res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
-
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Content-Disposition'
-//   );
-
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PUT, DELETE, OPTIONS'
-//   );
-
-//   next();
-// });
-
+//app listening!
 app.listen(
   port,
   console.log(`App listening on port ${port} in ${process.env.NODE_ENV}`)
