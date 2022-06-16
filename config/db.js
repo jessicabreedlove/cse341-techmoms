c1f7268ac5621b4df16a1fff7185c1670aa4598e
jessicabreedlove <jessicabreedlove@gmail.com>
2022-06-15 18:21:23 -0600

    console log good

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-console.log(`I am connecting to ${process.env.MONGO_URI}`);
 const connectDB = async () => {
+  console.log(`I am connecting to ${process.env.MONGO_URI}`);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
